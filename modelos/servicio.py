6d2693472333fc61855ea4f50818cb23d6df8d92
antuarjef-byte <antuarjef@gmail.com>
2026-05-12 20:51:14 -0500

    Create servicio.py

---
+++ modelos/servicio.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class Servicio(ABC):
+
+    def __init__(self, nombre, tarifa_base):
+
+        self.nombre = nombre
+        self.tarifa_base = tarifa_base
+
+    @abstractmethod
+    def calcular_costo(self):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
